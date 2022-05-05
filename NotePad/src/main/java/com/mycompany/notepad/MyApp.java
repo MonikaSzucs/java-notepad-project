54fd069ec878b4d2e0b8d90f532e811fae9c88a0
Monika Szucs <monika.silvia.s@hotmail.ca>
2022-05-05 04:27:53 -0700

    Setup Project Notepad

---
+++ NotePad/src/main/java/com/mycompany/notepad/MyApp.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.notepad;
+
+/**
+ *
+ * @author mszuc
+ */
+public class MyApp {
+
+    public static void main(String [] args){
+    
+    NotePad notp=new NotePad();
+}
+}
